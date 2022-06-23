79e833848c2739afb342d9b7b2fe7646d5431401
ericksicard <sicard.erick@gmail.com>
2022-06-22 21:57:02 -0400

    Tapping Into Context with the useContext Hook

--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -1,13 +1,11 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import AuthContext from '../../store/auth-context';
 
 import classes from './Navigation.module.css';
 
 const Navigation = (props) => {
-  return (
-    <AuthContext.Consumer>
-      {(ctx) =>{
-        return (
+  const ctx = useContext(AuthContext);
+  return (    
           <nav className={classes.nav}>
             <ul>
               {ctx.isLoggedIn && (
@@ -27,9 +25,6 @@ const Navigation = (props) => {
               )}
             </ul>
           </nav>
-        )
-      }}      
-    </AuthContext.Consumer>
   );
 };
 
